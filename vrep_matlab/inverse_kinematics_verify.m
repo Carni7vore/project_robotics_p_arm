31ff12dac58c671fe27928447c44c565e2e4be06
Carni7vore <xanthene1893@gmail.com>
2018-03-25 16:43:27 -0500

    P3 matlab
    3 position

--- vrep_matlab/inverse_kinematics_verify.m
+++ vrep_matlab/inverse_kinematics_verify.m
@@ -9,7 +9,7 @@ disp('Program started');
         disp('Connected to remote API server');
             
     opMode=vrep.simx_opmode_blocking;
-    [res,sphere]= vrep.simxGetObjectHandle(clientID,'Test',opMode);
+    [res,t1]= vrep.simxGetObjectHandle(clientID,'Test1',opMode);
     pause(1);
     if (res==vrep.simx_return_ok)
 %             fprintf('Position %d %d %d\n',x1,y1,z1);
@@ -18,32 +18,49 @@ disp('Program started');
             fprintf('Remote API function call returned with error code: %d\n',res);
     end
     
-    [res,position]=vrep.simxGetObjectPosition(clientID,sphere,-1,vrep.simx_opmode_blocking);
+    [res,position1]=vrep.simxGetObjectPosition(clientID,t1,-1,vrep.simx_opmode_blocking);
     pause(1);
+    
+    [res,t2]= vrep.simxGetObjectHandle(clientID,'Test2',opMode);
+    [res,position2]=vrep.simxGetObjectPosition(clientID,t2,-1,vrep.simx_opmode_blocking);
+    
+     [res,t3]= vrep.simxGetObjectHandle(clientID,'Test3',opMode);
+    [res,position3]=vrep.simxGetObjectPosition(clientID,t3,-1,vrep.simx_opmode_blocking);
 %      fprintf('position  %d\n',position);
-    if (res==vrep.simx_return_ok)
-            fprintf('position  %d\n',position);
-    else
-            fprintf('Remote API function call returned with error code: %d\n',res);
-    end
-    x1=position(1);
-    y1=position(2);
-    z1=position(3);
+%     if (res==vrep.simx_return_ok)
+%             fprintf('position  %d\n',position1);
+%     else
+%             fprintf('Remote API function call returned with error code: %d\n',res);
+%     end
+    x1=position1(1);
+    y1=position1(2);
+    z1=position1(3);
     
-%     x1=0.75;
-%     y1=0;
-%     z1=0.025;
+    x2=position2(1);
+    y2=position2(2);
+    z2=position2(3);
+    
+    x3=position3(1);
+    y3=position3(2);
+    z3=position3(3);
+    
     T1=[0 0 1 x1;0 1 0 y1; -1 0 0 z1; 0 0 0 1];
-    theta2=inverse_kinematics(T1);
+    T2=[0 0 1 x2;0 1 0 y2; -1 0 0 z2; 0 0 0 1];
+    T3=[0 0 1 x3;0 1 0 y3; -1 0 0 z3; 0 0 0 1];
+%     theta=[];
+    w=zeros(3,1);
+    theta1=inverse_kinematics(T1);
+    fprintf('theta %d \n',theta1);
+    w(1) = theta1;
+    theta2=inverse_kinematics(T2);
     fprintf('theta %d \n',theta2);
-    
+    w(2)= theta2;
+    theta3=inverse_kinematics(T3);
+    fprintf('theta %d \n',theta3);
+    w(3)= theta3;
 %     [res1,gripper]= vrep.simxGetObjectHandle(clientID,'BaxterGripper',opMode);
-    
-        
-       %theta 1 
-%     theta=[(theta(1)/180)*pi;(theta(2)/180)*pi;(theta(3)/180)*pi;(theta(4)/180)*pi;(theta(5)/180)*pi;(theta(6)/180)*pi];
-%     fprintf('theta %d \n', theta);
      %Get joints
+     
         opMode= vrep.simx_opmode_blocking;
         [res1,obj1]= vrep.simxGetObjectHandle(clientID,'P_Arm_joint1',opMode);
         [res2,obj2]= vrep.simxGetObjectHandle(clientID,'P_Arm_joint2',opMode);
@@ -51,9 +68,12 @@ disp('Program started');
         [res4,obj4]= vrep.simxGetObjectHandle(clientID,'P_Arm_joint4',opMode);
         [res5,obj5]= vrep.simxGetObjectHandle(clientID,'P_Arm_joint5',opMode);
         [res6,obj6]= vrep.simxGetObjectHandle(clientID,'P_Arm_joint6',opMode);
-        theta= real(single(theta2));
+        
+        for i=1:3
+            
+        theta= real(single(w(i)));
         target= forward_kinematics(theta);
-        target
+%         target
         fprintf('target position %d \n', target);
 %         %Move joints
         res=vrep.simxSetJointTargetPosition(clientID,obj1,theta(1), vrep.simx_opmode_blocking);
@@ -67,13 +87,8 @@ disp('Program started');
         res=vrep.simxSetJointTargetPosition(clientID,obj5,theta(5), vrep.simx_opmode_blocking);
         pause(1);
         res=vrep.simxSetJointTargetPosition(clientID,obj6,theta(6), vrep.simx_opmode_blocking);
-         pause(3);
-%       
-%         
-
-        pause(2);
-    
-       
+        pause(3);
+        end   
         % Now send some data to V-REP in a non-blocking fashion:
         vrep.simxAddStatusbarMessage(clientID,'Hello V-REP!',vrep.simx_opmode_oneshot);
 
