fd516d70bd5c10856d90f10a772704b2f9b8e1b0
Carni7vore <xanthene1893@gmail.com>
2018-03-31 13:12:32 -0500

    p4 test forrward
    add bill
    add movement

--- vrep_matlab/test_forward.m
+++ vrep_matlab/test_forward.m
@@ -10,9 +10,20 @@ function test_forward()
         disp('Connected to remote API server');
             
         % Now try to retrieve data in a blocking fashion (i.e. a service call):
-        
-        
-        theta=[(90/180)*pi;(90/180)*pi;0;0;0;0];
+     %theta1=135;
+     %theta2=110;
+     %theta3=115;
+     %theta4=165;
+     %theta5=115;
+     %theta6=160;
+        for i=1:20
+            
+     
+            theta1=[20+i*3;30-i*5; 0+i*5;-100+i*5;6*i-50;90-10*i];
+        theta=theta1/180*pi;
+        pos=[0.2+2i;3-6i;4+8i];
+       [res, bill]=vrep.simxGetObjectHandle(clientID,'Bill_goalDummy',opMode);
+        res= vrep.simxSetObjectPosition(clientID,bill,-1,pos,opMode);
         
        
         
@@ -27,20 +38,21 @@ function test_forward()
         
         
         res=vrep.simxSetJointTargetPosition(clientID,obj1,theta(1), vrep.simx_opmode_streaming);
-        pause(1);
+%         pause(1);
         res=vrep.simxSetJointTargetPosition(clientID,obj2,theta(2), vrep.simx_opmode_streaming);
-        pause(1);
+%         pause(1);
         res=vrep.simxSetJointTargetPosition(clientID,obj3,theta(3), vrep.simx_opmode_streaming);
-        pause(1);
+%         pause(1);
         res=vrep.simxSetJointTargetPosition(clientID,obj4,theta(4), vrep.simx_opmode_streaming);
-        pause(1);
+%         pause(1);
         res=vrep.simxSetJointTargetPosition(clientID,obj5,theta(5), vrep.simx_opmode_streaming);
-        pause(1);
+%         pause(1);
         res=vrep.simxSetJointTargetPosition(clientID,obj6,theta(6), vrep.simx_opmode_streaming);
+        disp(theta1);
+        pause(1);
         
+        end
         
-        
-          pause(1);
 %         if (res==vrep.simx_return_ok)
 %             fprintf('%d',theta(1));
 %          else
@@ -56,43 +68,17 @@ function test_forward()
 %                 fprintf('Remote API function call returned with error code: %d\n',res);
 %             end
 %         end
-        pause(2);
-        opMode=vrep.simx_opmode_blocking;
-         
-        [res,objs]=vrep.simxGetObjects(clientID,vrep.sim_handle_all,vrep.simx_opmode_blocking);
-        [res1,obj1]= vrep.simxGetObjectHandle(clientID,'P_Arm_joint1',opMode);
-        [res2,obj2]= vrep.simxGetObjectHandle(clientID,'P_Arm_joint2',opMode);
-        [res3,obj3]= vrep.simxGetObjectHandle(clientID,'P_Arm_joint3',opMode);
-        [res4,obj4]= vrep.simxGetObjectHandle(clientID,'P_Arm_joint4',opMode);
-        [res5,obj5]= vrep.simxGetObjectHandle(clientID,'P_Arm_joint5',opMode);
-        [res6,obj6]= vrep.simxGetObjectHandle(clientID,'P_Arm_joint6',opMode);
-        [res,angle1]=vrep.simxGetJointPosition(clientID,obj1, opMode);
-        [res,angle2]=vrep.simxGetJointPosition(clientID,obj2, opMode);
-        [res,angle3]=vrep.simxGetJointPosition(clientID,obj3, opMode);
-        [res,angle4]=vrep.simxGetJointPosition(clientID,obj4, opMode);
-        [res,angle5]=vrep.simxGetJointPosition(clientID,obj5, opMode);
-        [res,angle6]=vrep.simxGetJointPosition(clientID,obj6, opMode);
-        theta(1)=angle1;
-        theta(2)= angle2;
-        theta(3)=angle3;
-        theta(4)= angle4;
-        theta(5)=angle5;
-        theta(6)=angle6;
-%         jointHandles[i]=sim.getObjectHandle('P_Arm_joint'..i)
-        if (res==vrep.simx_return_ok)
-            fprintf('Angles %d\n',theta);
-        else
-            fprintf('Remote API function call returned with error code: %d\n',res);
-        end
-            
-        pause(2);
+      
+%         opMode=vrep.simx_opmode_blocking;
+%          
+%     
     
        
         % Now send some data to V-REP in a non-blocking fashion:
-        vrep.simxAddStatusbarMessage(clientID,'Hello V-REP!',vrep.simx_opmode_oneshot);
+%         vrep.simxAddStatusbarMessage(clientID,'Hello V-REP!',vrep.simx_opmode_oneshot);
 
         % Before closing the connection to V-REP, make sure that the last command sent out had time to arrive. You can guarantee this with (for example):
-        vrep.simxGetPingTime(clientID);
+%         vrep.simxGetPingTime(clientID);
 
         % Now close the connection to V-REP:    
         vrep.simxFinish(clientID);
