9539933f781a36f145890ed752ae60259bddde5b
jingwu6 <37253624+jingwu6@users.noreply.github.com>
2018-04-28 17:11:35 -0500

    update

---
+++ vrep_matlab/write2.m
@@ -0,0 +1,171 @@
+%final project step 1
+
+
+disp('Program started');
+
+% vrep=remApi('remoteApi','extApi.h'); % using the header (requires a compiler)
+vrep=remApi('remoteApi'); % using the prototype file (remoteApiProto.m)
+vrep.simxFinish(-1); % just in case, close all opened connections
+clientID=vrep.simxStart('127.0.0.1',19999,true,true,5000,5);
+x1=-0.7;
+y1=-0.335+0.6;
+z1=0.85;
+
+
+if (clientID>-1)
+    disp('Connected to remote API server');
+
+    opMode= vrep.simx_opmode_blocking;
+    [res1,obj1]= vrep.simxGetObjectHandle(clientID,'P_Arm_joint1',opMode);
+    [res2,obj2]= vrep.simxGetObjectHandle(clientID,'P_Arm_joint2',opMode);
+    [res3,obj3]= vrep.simxGetObjectHandle(clientID,'P_Arm_joint3',opMode);
+    [res4,obj4]= vrep.simxGetObjectHandle(clientID,'P_Arm_joint4',opMode);
+    [res5,obj5]= vrep.simxGetObjectHandle(clientID,'P_Arm_joint5',opMode);
+    [res6,obj6]= vrep.simxGetObjectHandle(clientID,'P_Arm_joint6',opMode);
+    theta0=zeros(6,47);
+    
+    t=[1 0 0 -0.35;0 1 0 -0.00013;0 0 1 1.2552;0 0 0 1];
+    T1=[0 0 -1 (x1+0.1);0 1 0 y1; 1 0 0 z1; 0 0 0 1];
+    theta1=inverse_kinematics(T1,t);
+    theta= real(single(theta1));
+    theta0(1:6,1)=theta;
+    t=T1;
+    i=1;
+    res=vrep.simxSetJointTargetPosition(clientID,obj1,theta0(1,i), vrep.simx_opmode_blocking);   
+    res=vrep.simxSetJointTargetPosition(clientID,obj2,theta0(2,i), vrep.simx_opmode_blocking);   
+    res=vrep.simxSetJointTargetPosition(clientID,obj3,theta0(3,i), vrep.simx_opmode_blocking);  
+    res=vrep.simxSetJointTargetPosition(clientID,obj4,theta0(4,i), vrep.simx_opmode_blocking);
+    res=vrep.simxSetJointTargetPosition(clientID,obj5,theta0(5,i), vrep.simx_opmode_blocking);
+    res=vrep.simxSetJointTargetPosition(clientID,obj6,theta0(6,i), vrep.simx_opmode_blocking);
+     x1=-0.7;
+    for i=2:10
+        y1= y1+0.07/8;
+        T1=[0 0 -1 x1;0 1 0 y1; 1 0 0 z1; 0 0 0 1];
+        theta1=inverse_kinematics(T1,t);
+        theta= real(single(theta1));
+        theta0(1:6,i)=theta;
+        t=T1;
+        res=vrep.simxSetJointTargetPosition(clientID,obj1,theta0(1,i), vrep.simx_opmode_blocking);   
+        res=vrep.simxSetJointTargetPosition(clientID,obj2,theta0(2,i), vrep.simx_opmode_blocking);   
+        res=vrep.simxSetJointTargetPosition(clientID,obj3,theta0(3,i), vrep.simx_opmode_blocking);  
+        res=vrep.simxSetJointTargetPosition(clientID,obj4,theta0(4,i), vrep.simx_opmode_blocking);
+        res=vrep.simxSetJointTargetPosition(clientID,obj5,theta0(5,i), vrep.simx_opmode_blocking);
+        res=vrep.simxSetJointTargetPosition(clientID,obj6,theta0(6,i), vrep.simx_opmode_blocking);
+    end
+   
+    for i=11:14
+        z1=z1-0.15/8;
+        T1=[0 0 -1 x1;0 1 0 y1; 1 0 0 z1; 0 0 0 1];
+        theta1=inverse_kinematics(T1,t);
+        theta= real(single(theta1));
+        theta0(1:6,i)=theta;
+        t=T1;
+        res=vrep.simxSetJointTargetPosition(clientID,obj1,theta0(1,i), vrep.simx_opmode_blocking);   
+        res=vrep.simxSetJointTargetPosition(clientID,obj2,theta0(2,i), vrep.simx_opmode_blocking);   
+        res=vrep.simxSetJointTargetPosition(clientID,obj3,theta0(3,i), vrep.simx_opmode_blocking);  
+        res=vrep.simxSetJointTargetPosition(clientID,obj4,theta0(4,i), vrep.simx_opmode_blocking);
+        res=vrep.simxSetJointTargetPosition(clientID,obj5,theta0(5,i), vrep.simx_opmode_blocking);
+        res=vrep.simxSetJointTargetPosition(clientID,obj6,theta0(6,i), vrep.simx_opmode_blocking);
+    end
+           for i=15:18
+        z1=z1-0.15/8;
+        x1=-0.6;
+        T1=[0 0 -1 x1;0 1 0 y1; 1 0 0 z1; 0 0 0 1];
+        theta1=inverse_kinematics(T1,t);
+        theta= real(single(theta1));
+        theta0(1:6,i)=theta;
+        t=T1;
+        res=vrep.simxSetJointTargetPosition(clientID,obj1,theta0(1,i), vrep.simx_opmode_blocking);   
+        res=vrep.simxSetJointTargetPosition(clientID,obj2,theta0(2,i), vrep.simx_opmode_blocking);   
+        res=vrep.simxSetJointTargetPosition(clientID,obj3,theta0(3,i), vrep.simx_opmode_blocking);  
+        res=vrep.simxSetJointTargetPosition(clientID,obj4,theta0(4,i), vrep.simx_opmode_blocking);
+        res=vrep.simxSetJointTargetPosition(clientID,obj5,theta0(5,i), vrep.simx_opmode_blocking);
+        res=vrep.simxSetJointTargetPosition(clientID,obj6,theta0(6,i), vrep.simx_opmode_blocking);
+    end
+    for i=19:26
+        y1= y1-0.07/8;
+        x1=-0.7;
+        T1=[0 0 -1 x1;0 1 0 y1; 1 0 0 z1; 0 0 0 1];
+        theta1=inverse_kinematics(T1,t);
+        theta= real(single(theta1));
+        theta0(1:6,i)=theta;
+        t=T1;
+        res=vrep.simxSetJointTargetPosition(clientID,obj1,theta0(1,i), vrep.simx_opmode_blocking);   
+        res=vrep.simxSetJointTargetPosition(clientID,obj2,theta0(2,i), vrep.simx_opmode_blocking);   
+        res=vrep.simxSetJointTargetPosition(clientID,obj3,theta0(3,i), vrep.simx_opmode_blocking);  
+        res=vrep.simxSetJointTargetPosition(clientID,obj4,theta0(4,i), vrep.simx_opmode_blocking);
+        res=vrep.simxSetJointTargetPosition(clientID,obj5,theta0(5,i), vrep.simx_opmode_blocking);
+        res=vrep.simxSetJointTargetPosition(clientID,obj6,theta0(6,i), vrep.simx_opmode_blocking);
+    end
+    for i=27:30
+        z1=z1+0.15/8;
+        T1=[0 0 -1 x1;0 1 0 y1; 1 0 0 z1; 0 0 0 1];
+        theta1=inverse_kinematics(T1,t);
+        theta= real(single(theta1));
+        theta0(1:6,i)=theta;
+        t=T1;
+        res=vrep.simxSetJointTargetPosition(clientID,obj1,theta0(1,i), vrep.simx_opmode_blocking);   
+        res=vrep.simxSetJointTargetPosition(clientID,obj2,theta0(2,i), vrep.simx_opmode_blocking);   
+        res=vrep.simxSetJointTargetPosition(clientID,obj3,theta0(3,i), vrep.simx_opmode_blocking);  
+        res=vrep.simxSetJointTargetPosition(clientID,obj4,theta0(4,i), vrep.simx_opmode_blocking);
+        res=vrep.simxSetJointTargetPosition(clientID,obj5,theta0(5,i), vrep.simx_opmode_blocking);
+        res=vrep.simxSetJointTargetPosition(clientID,obj6,theta0(6,i), vrep.simx_opmode_blocking);
+    end
+       
+   
+    
+    for i=31:38
+        y1=y1+0.07/8;
+        T1=[0 0 -1 x1;0 1 0 y1; 1 0 0 z1; 0 0 0 1];
+        theta1=inverse_kinematics(T1,t);
+        theta= real(single(theta1));
+        theta0(1:6,i)=theta;
+        t=T1;
+        res=vrep.simxSetJointTargetPosition(clientID,obj1,theta0(1,i), vrep.simx_opmode_blocking);   
+        res=vrep.simxSetJointTargetPosition(clientID,obj2,theta0(2,i), vrep.simx_opmode_blocking);   
+        res=vrep.simxSetJointTargetPosition(clientID,obj3,theta0(3,i), vrep.simx_opmode_blocking);  
+        res=vrep.simxSetJointTargetPosition(clientID,obj4,theta0(4,i), vrep.simx_opmode_blocking);
+        res=vrep.simxSetJointTargetPosition(clientID,obj5,theta0(5,i), vrep.simx_opmode_blocking);
+        res=vrep.simxSetJointTargetPosition(clientID,obj6,theta0(6,i), vrep.simx_opmode_blocking);
+    end
+    i=39;
+    T1=[0 0 -1 x1+0.1;0 1 0 y1; 1 0 0 z1; 0 0 0 1];
+    theta1=inverse_kinematics(T1,t);
+    theta= real(single(theta1));
+    theta0(1:6,39)=theta;
+    t=T1;
+    
+    res=vrep.simxSetJointTargetPosition(clientID,obj1,theta0(1,i), vrep.simx_opmode_blocking);   
+    res=vrep.simxSetJointTargetPosition(clientID,obj2,theta0(2,i), vrep.simx_opmode_blocking);   
+    res=vrep.simxSetJointTargetPosition(clientID,obj3,theta0(3,i), vrep.simx_opmode_blocking);  
+    res=vrep.simxSetJointTargetPosition(clientID,obj4,theta0(4,i), vrep.simx_opmode_blocking);
+    res=vrep.simxSetJointTargetPosition(clientID,obj5,theta0(5,i), vrep.simx_opmode_blocking);
+    res=vrep.simxSetJointTargetPosition(clientID,obj6,theta0(6,i), vrep.simx_opmode_blocking);
+%     disp(theta);
+%     for k=1:30
+%    res=vrep.simxSetJointTargetPosition(clientID,obj1,theta0(1,k), vrep.simx_opmode_blocking);   
+%     res=vrep.simxSetJointTargetPosition(clientID,obj2,theta0(2,k), vrep.simx_opmode_blocking);   
+%     res=vrep.simxSetJointTargetPosition(clientID,obj3,theta0(3,k), vrep.simx_opmode_blocking);  
+%     res=vrep.simxSetJointTargetPosition(clientID,obj4,theta0(4,k), vrep.simx_opmode_blocking);
+%     res=vrep.simxSetJointTargetPosition(clientID,obj5,theta0(5,k), vrep.simx_opmode_blocking);
+%     res=vrep.simxSetJointTargetPosition(clientID,obj6,theta0(6,k), vrep.simx_opmode_blocking);
+%     pause(0.5);
+%     end
+    
+    vrep.simxAddStatusbarMessage(clientID,'Hello V-REP!',vrep.simx_opmode_oneshot);
+
+    % Before closing the connection to V-REP, make sure that the last command sent out had time to arrive. You can guarantee this with (for example):
+    vrep.simxGetPingTime(clientID);
+
+    % Now close the connection to V-REP:    
+    vrep.simxFinish(clientID);
+
+else
+    disp('Failed connecting to remote API server');
+end
+vrep.delete(); % call the destructor!
+THETA2{4}=theta0;
+
+disp('Program ended');
+
+
