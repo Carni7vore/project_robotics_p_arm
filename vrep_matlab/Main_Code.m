12d45946003fbf94a8b63494517457d8cb5e1a9b
jingwu6 <37253624+jingwu6@users.noreply.github.com>
2018-04-29 17:34:42 -0500

    update

--- vrep_matlab/Main_Code.m
+++ vrep_matlab/Main_Code.m
@@ -1,3 +1,5 @@
+while 1
+
 Judge=input('What can i do for you?\n','s');
 work1=norm(strfind(Judge, 'time'));
 work2=norm(str2num(Judge));
@@ -5,6 +7,12 @@ work2=norm(str2num(Judge));
 % A=isstrprop(Judge,'digit');
 % B=str(A);
 % C=str2num(B)
+count=1;
+
+if count~=1
+    
+end
+
 
     if work1~=0
     write_num;
@@ -13,3 +21,7 @@ work2=norm(str2num(Judge));
     if work2~=0
             calculation_final(str2num(Judge));
     end
+    
+    count=count+1;
+    
+end
